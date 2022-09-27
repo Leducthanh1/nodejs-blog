28b51ef3537c56cc9bd4ff1e2192434b41a1b2fb
Leducthanh1 <ronthanhyal@gmail.com>
2022-09-27 10:37:03 +0700

    Select all with Checkbox

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -2,13 +2,16 @@ const { restart } = require('nodemon');
 const Course = require('../models/Course');
 const { multipleMongooseToObject } = require('../../util/mongoose');
 const { mongooseToObject } = require('../../util/mongoose');
+const { Promise } = require('mongoose');
 class MeController{
     //[GET]me/stored/courses
     storedCourses(req, res, next){
-        Course.find({})
-        .then(courses => {
-            res.render('me/stored-courses',
-            {courses: multipleMongooseToObject(courses)});
+        Promise.all([Course.find(), Course.countDocumentsDeleted()])
+        .then(([courses, deletedCount]) =>{
+            res.render('me/stored-courses',{
+                deletedCount,
+                courses: multipleMongooseToObject(courses)
+            })
         })
         .catch(next)
     }

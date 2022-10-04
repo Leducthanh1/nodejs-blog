ed406684ad4dcc501ff9ba6b890c75274b0d601a
Leducthanh1 <ronthanhyal@gmail.com>
2022-10-04 10:37:41 +0700

    Auto increment

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
 const mongooseDelete = require('mongoose-delete');
-
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema({
+    _id: {type: Number},
     name: { type : String, required: true,},
     description: { type : String},
     image: { type : String},
@@ -13,6 +14,7 @@ const CourseSchema = new Schema({
     level: { type : String},
     slug: { type: String, slug: 'name', unique:true }
 }, {
+    _id: false,
     timestamps: true,
 });
 
@@ -31,6 +33,7 @@ CourseSchema.query.sortable = function(req){
 //add plugins
 
 mongoose.plugin(slug);
+CourseSchema.plugin(AutoIncrement);
 CourseSchema.plugin(mongooseDelete, { 
     deletedAt : true,
     overrideMethods : 'all',
